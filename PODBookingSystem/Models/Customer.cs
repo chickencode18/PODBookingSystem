39d8fe68822696762c50a1bc729bbcb8b49b3018
Nguyễn Võ Đăng Khoa <162529207+chickencode18@users.noreply.github.com>
2024-11-21 23:37:19 +0700

    Delete PODBookingSystem directory

--- PODBookingSystem/Models/Customer.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PODBookingSystem.Models
-{
-    public class Customer : User
-    {
-        public bool IsVIP { get; set; } = false; // Kiểm tra khách hàng VIP
-        public new string Role { get; set; }
-    }
-}
--- PODBookingSystem/Models/Report.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PODBookingSystem.Models
-{
-    public class Report
-    {
-        public int Id { get; set; }
-        public DateTime ReportDate { get; set; }
-        public string Content { get; set; }
-    }
-}
